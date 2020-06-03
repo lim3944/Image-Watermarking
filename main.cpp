1229c801590df4d099e8f632849ba83f5806b87e
Tak <34337910+lim3944@users.noreply.github.com>
2020-06-03 19:28:54 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,143 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<string>
+#include<algorithm>
+
+// opencv header
+#include "opencv2\highgui.hpp"
+#include "opencv2\videoio.hpp"
+#include "opencv2\opencv.hpp"
+
+// m sequence
+#include "mls.h"
+
+#define M_PI 3.14159265358979323846
+
+using namespace std;
+using namespace cv;
+
+// png image to jpg with watermark & key
+cv::Mat Encoder(cv::Mat, string, int);
+cv::Mat array2block(vector<int>, int, int);
+
+// jpg image to png seperating image and watermark with key
+pair<cv::Mat, string> Decoder(cv::Mat, int);
+vector<int> block2array(cv::Mat, int, int);
+
+
+// m-sequence for sync, message
+vector<bool> sync_seq;
+vector<bool> message_seq;
+
+int main() {
+
+	string watermark;
+	int key = 0;
+	cv::Mat img;
+	img = cv::imread("1.png"); // image name
+
+	// Watermark and key value setting
+	while (watermark.length() != 12) {
+		printf("Enter 12 letters watermark :");
+		cin >> watermark;
+	}
+
+	printf("Enter key value : ");
+	cin >> key;
+
+	// m-sequence generator
+	mls mls_aes(6, true);
+	mls mls_rnd(10);
+
+	sync_seq = mls_rnd.get_seq();
+	sync_seq.push_back(1);
+	message_seq = mls_aes.get_seq();
+	message_seq.push_back(1);
+
+	// Encoding
+	cv::Mat img_encoded;
+	img_encoded = Encoder(img, watermark, key);
+
+	//cv::imshow("marked img", img_encoded);
+	//cv::imwrite("makred_img.jpg, img_encoded);
+
+	// Decoding
+	pair <cv::Mat, string> temp;
+	cv::Mat img_decoded;
+	string wm_decoded;
+	temp = Decoder(img_encoded, key);
+	img_decoded = temp.first;
+	wm_decoded = temp.second;
+
+	//cv::imshow("Decoded img", img_decoded);
+	//printf("Decoded Watermark Message : %s\n",wm_decoded);
+	
+	while (1) {
+		char key = (char)cv::waitKey(10);
+		if (key == 27)
+			break;
+	}
+	return 0;
+}
+
+// Encoding watermkar to png img with key value, and change to jpeg format
+cv::Mat Encoder(cv::Mat img, string watermark, int key) {
+
+	// making sequence array for watermark
+	vector<bool> wm_seq;
+	wm_seq = sync_seq;
+	// shifting m-sequence for watermark
+	for (int i = 0; i < watermark.length(); i++) {
+		int message = watermark[i];
+		for (int j = 0; j < 256; j++) {
+			wm_seq.push_back(message_seq[message++]);
+			if (message > 256)
+				message = 0;
+		}
+	}
+
+	cv::Mat block = array2block(wm_seq, 64, key);
+
+	for (int i = 0; i < img.rows; i += 64) {
+		for (int j = 0; j < img.cols; j += 64) {
+
+		}
+	}
+
+	return img;
+}
+
+// 1D array to 2D block matching
+cv::Mat array2block(vector<bool> rblock, int size, int key) {
+	cv::Mat block = cv::Mat(cv::Size(size, size), CV_8UC1);
+	
+	vector<int> temp(rblock.begin(), rblock.end());
+
+	int loc = 0;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			block.at<int>(i, j) = (temp[loc] == 0 ? 1 : -1);
+			loc += key;
+			if (loc >= size)
+				loc %= size;
+		}
+	}
+	
+	return block;
+}
+
+// Decoding image, seperating into watermark and png image from jpeg image
+pair<cv::Mat, string > Decoder(cv::Mat img, int key) {
+	return { img, "asdfasd" };
+}
+
+// 2D block to 1D array matching
+vector<int> block2array(cv::Mat block, int size, int key) {
+	vector<int> barray(size * size);
+
+	return barray;
+}
+
+
